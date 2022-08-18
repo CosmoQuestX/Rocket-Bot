a24f6fbffdb7711fd6553595a0d21bb2cf5da5f6
The Real Fake Admin <therealfakedev@gmail.com>
2022-08-17 23:49:22 -0400

    corrected calc.js

--- CosmoQuestBot-master/commands/calc.js
+++ CosmoQuestBot-master/commands/calc.js
@@ -2,9 +2,9 @@ var math = require("mathjs");
 exports.run = function calc (client, msg, args) {
   try {
     if (args.length < 1) throw new RangeError(`${args.length} arguments passed`);
-    if ((args == "π") || (args == "pi")) {
+    if ((args[0] == "π") || (args[0] == "pi")) {
       msg.channel.send("3.141592653589793238462643383279502884197169");
-    } else if (args == "p=np") {
+    } else if (args[0] == "p=np") {
       msg.channel.send("An answer to the P versus NP question would determine whether problems that can be verified in polynomial time can also be solved in polynomial time.");
       msg.channel.send("If it turns out that P ≠ NP, which is widely believed, it would mean that there are problems in NP that are harder to compute than to verify: they could not be solved in polynomial time, but the answer could be verified in polynomial time.");
       msg.channel.send("A proof that P = NP could have stunning practical consequences if the proof leads to efficient methods for solving some of the important problems in NP.");
