7324bce143580e0f9eb507a96cb611f7dde2f55f
Gordon Dewis <gordon@dewis.ca>
2022-08-23 23:44:35 -0400

    Flatten the XML

--- CosmoQuestBot-master/commands/metar.js
+++ CosmoQuestBot-master/commands/metar.js
@@ -103,7 +103,8 @@ exports.run = function metar (_, msg, args) {
             // convert it to a JSON string
             const json = JSON.stringify(result, null, 4);
 
-            msg.channel.send("Result: "  + json);
+//            msg.channel.send("Result: "  + json);
+            msg.channel.send("Result: " + json.raw_text);
 
         });
  //       msg.channel.send(body);
